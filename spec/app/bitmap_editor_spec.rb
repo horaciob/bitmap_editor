002d460f5498b6825db4294210e25d0690b81830
Hora b <horaciob@gmail.com>
2016-09-10 16:45:23 -0300

    silence stdout on editor test

--- spec/app/bitmap_editor_spec.rb
+++ spec/app/bitmap_editor_spec.rb
@@ -2,6 +2,7 @@
 require_relative '../spec_helper.rb'
 
 RSpec.describe BitmapEditor do
+
   let(:console) { described_class.new }
 
   context '.exit_console' do
@@ -13,47 +14,56 @@ RSpec.describe BitmapEditor do
 
   context '.run' do
     it 'runs exit command' do
+      
+      allow($stdout).to receive(:write) # this avoid to show sdout message while rspec runs
       allow(Readline).to receive(:readline) { 'X' }
       console.run
     end
 
     it 'runs create command' do
+      allow($stdout).to receive(:write) 
       allow(CreateCommand).to receive(:new).and_return(instance_double(CreateCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('I 2 2', 'X')
       console.run
     end
 
     it 'runs show command' do
+      allow($stdout).to receive(:write) 
       allow(ShowCommand).to receive(:new).and_return(instance_double(ShowCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('S', 'X')
       console.run
     end
 
     it 'runs Clear command' do
+      allow($stdout).to receive(:write) 
       allow(ClearCommand).to receive(:new).and_return(instance_double(ClearCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('C', 'X')
       console.run
     end
 
     it 'runs Change command' do
+      allow($stdout).to receive(:write) 
       allow(ChangeCommand).to receive(:new).and_return(instance_double(ChangeCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('L 2 3 A', 'X')
       console.run
     end
 
     it 'runs Vertical command' do
+      allow($stdout).to receive(:write) 
       allow(VerticalCommand).to receive(:new).and_return(instance_double(VerticalCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('V 2 3 6 W', 'X')
       console.run
     end
 
     it 'runs Horizontal Command' do
+      allow($stdout).to receive(:write) 
       allow(HorizontalCommand).to receive(:new).and_return(instance_double(HorizontalCommand, execute: 0))
       allow(Readline).to receive(:readline).and_return('H 3 5 2 Z', 'X')
       console.run
     end
 
     it 'show unknown command' do
+      allow($stdout).to receive(:write) 
       allow(Readline).to receive(:readline).and_return('FAKECOMMAND', 'X')
       expect { console.run }.to output(/unrecognised command/).to_stdout
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,11 @@ SimpleCov.refuse_coverage_drop
 RSpec.configure do |config|
   config.color = true
   config.formatter = :documentation
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
+  
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
