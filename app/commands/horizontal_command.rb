aea3f91570874514cda786634fa945532bd77666
Hora b <horaciob@gmail.com>
2016-09-10 11:15:15 -0300

    fix validation

--- app/commands/horizontal_command.rb
+++ app/commands/horizontal_command.rb
@@ -5,9 +5,9 @@ class HorizontalCommand < Command
   def validate(bitmap, args)
     raise ValidationError, 'Check your arguments' if args.size != 5
     raise ValidationError, 'You have to create a new bitmap first' if bitmap.nil?
-    raise ValidationError, 'Arg X1 is invalid' unless bitmap.valid_index?(args[1])
-    raise ValidationError, 'Arg X2 is invalid' unless bitmap.valid_index?(args[2])
-    raise ValidationError, 'Arg Y is invalid' unless bitmap.valid_index?(1, args[3])
+    raise ValidationError, 'Arg X1 is invalid' unless bitmap.valid_index?(1,args[1])
+    raise ValidationError, 'Arg X2 is invalid' unless bitmap.valid_index?(1,args[2])
+    raise ValidationError, 'Arg Y is invalid' unless bitmap.valid_index?(args[3])
   end
 
   def run(bitmap, *args)
