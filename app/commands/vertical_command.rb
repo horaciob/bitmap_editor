7904d0311d43eafe541d42acf090324b2d5bf554
Hora b <horaciob@gmail.com>
2016-09-10 11:10:56 -0300

    add all validations

--- app/commands/vertical_command.rb
+++ app/commands/vertical_command.rb
@@ -2,8 +2,12 @@
 
 # Create Matrix command
 class VerticalCommand < Command
-  def validate(bitmap, _args)
+  def validate(bitmap, args)
+    raise ValidationError, 'Check your arguments' if args.size != 5
     raise ValidationError, 'You have to create a new bitmap first' if bitmap.nil?
+    raise ValidationError, 'Arg X is invalid' unless bitmap.valid_index?(1, args[1])
+    raise ValidationError, 'Arg Y1 is invalid' unless bitmap.valid_index?(args[2])
+    raise ValidationError, 'Arg Y2 is invalid' unless bitmap.valid_index?(args[3])
   end
 
   def run(bitmap, *args)
--- spec/app/commands/vertical_command_spec.rb
+++ spec/app/commands/vertical_command_spec.rb
@@ -2,9 +2,9 @@
 require_relative '../../spec_helper.rb'
 
 RSpec.describe VerticalCommand do
+  let(:bitmap){Bitmap.new(5,6)}
   context '.execute' do 
     it 'creates a new Bitmap' do 
-      bitmap = Bitmap.new(5,6)
       described_class.new.execute(bitmap,"V 2 3 6 W")
       expect(bitmap.matrix).to contain_exactly(%w(O O O O O),
                                                %w(O O O O O),
@@ -17,7 +17,22 @@ RSpec.describe VerticalCommand do
 
   context '.validations' do
     it 'throws  ValidationError when run without bitmap' do
-      expect { described_class.new.validate(nil, %w(I N 1)) }.to raise_error ValidationError
+      expect { described_class.new.validate(nil, %w(V 2 3 6 W)) }.to raise_error ValidationError, 'You have to create a new bitmap first' 
+    end
+    it 'raise error when have less args' do
+      expect { described_class.new.validate(bitmap, %w(V 2 6 W)) }.to raise_error ValidationError, 'Check your arguments'
+    end
+    it 'throws  ValidationError when has more args' do
+      expect { described_class.new.validate(bitmap, %w(V 2 3 6 W P)) }.to raise_error ValidationError,'Check your arguments'
+    end
+    it 'fails when X is not valid' do
+      expect { described_class.new.validate(bitmap, %w(V 8 3 6 W)) }.to raise_error ValidationError, 'Arg X is invalid'
+    end
+    it 'fails when Y1 is not valid' do
+      expect { described_class.new.validate(bitmap, %w(V 2 10 6 W)) }.to raise_error ValidationError, 'Arg Y1 is invalid' 
+    end
+    it 'fails when Y2 is not valid' do
+      expect { described_class.new.validate(bitmap, %w(V 2 3 10 W)) }.to raise_error ValidationError, 'Arg Y2 is invalid'
     end
   end
 end
